a89f41feeaf35ccab217ae462dc410d178e28946
James Wang <jw@slickage.com>
2015-11-18 19:35:43 -1000

    Message Model

---
+++ priv/repo/migrations/20151119030511_create_message.exs
@@ -0,0 +1,13 @@
+defmodule Hello.Repo.Migrations.CreateMessage do
+  use Ecto.Migration
+
+  def change do
+    create table(:messages) do
+      add :body, :string
+      add :author, :string
+
+      timestamps
+    end
+
+  end
+end
